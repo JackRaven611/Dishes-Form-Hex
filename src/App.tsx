e91e04d50ec74a8a93afedb1b1c9d453aca62557
Jakub Bedynek <kuba.xray@gmail.com>
2023-05-11 17:44:34 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { TypeFields } from "./components/typeFields/typeFields";
+import "./App.css";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    preparation_time: "",
+    type: "pizza",
+    no_of_slices: "",
+    diameter: "",
+    spiciness: "",
+    slices_of_bread: "",
+  });
+
+  const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setInputs({
+      ...inputs,
+      type: event.target.value,
+      no_of_slices: "",
+      diameter: "",
+      spiciness: "",
+      slices_of_bread: "",
+    });
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, name: event.target.value });
+  };
+  const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, preparation_time: event.target.value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form action="">
+        <ul>
+          <li>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Name"
+              maxLength={35}
+              onChange={handleNameChange}
+              required
+            />
+          </li>
+          <li>
+            <label htmlFor="prepTime">Preparation Time</label>
+            <input
+              id="prepTime"
+              type="time"
+              step={1}
+              onChange={handleTimeChange}
+              required
+            />
+          </li>
+          <li>
+            <label htmlFor="dishType">Type of Dish</label>
+            <select
+              id="dishType"
+              value={inputs.type}
+              onChange={handleTypeChange}
+            >
+              <option value="pizza">Pizza</option>
+
+              <option value="soup">Soup</option>
+
+              <option value="sandwich">Sandwich</option>
+            </select>
+          </li>
+          <li>
+            <TypeFields inputs={inputs} setInputs={setInputs} />
+          </li>
+        </ul>
+
+        <button
+          type="button"
+          onClick={() => {
+            console.log(inputs);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/components/typeFields/typeFields.tsx
@@ -0,0 +1,103 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+interface TypeFieldsProps {
+  inputs: {
+    name: string;
+    preparation_time: string;
+    type: string;
+    no_of_slices: string;
+    diameter: string;
+    spiciness: string;
+    slices_of_bread: string;
+  };
+  setInputs: Dispatch<
+    SetStateAction<{
+      name: string;
+      preparation_time: string;
+      type: string;
+      no_of_slices: string;
+      diameter: string;
+      spiciness: string;
+      slices_of_bread: string;
+    }>
+  >;
+}
+
+export const TypeFields = ({ inputs, setInputs }: TypeFieldsProps) => {
+  const handleSlicesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, no_of_slices: event.target.value });
+  };
+  const handleDiameterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, diameter: event.target.value });
+  };
+  const handleSpicinessChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputs({ ...inputs, spiciness: event.target.value });
+  };
+  const handleBreadChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, slices_of_bread: event.target.value });
+  };
+
+  if (inputs.type === "pizza") {
+    return (
+      <>
+        <div>
+          <label htmlFor="slices">Number of Slices</label>
+          <input
+            id="slices"
+            type="number"
+            min={1}
+            max={35}
+            placeholder="Number of Slices"
+            onChange={handleSlicesChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="diameter">Diameter</label>
+          <input
+            id="diameter"
+            type="number"
+            step="0.1"
+            min={10}
+            max={50}
+            placeholder="Diameter"
+            onChange={handleDiameterChange}
+            required
+          />
+        </div>
+      </>
+    );
+  } else if (inputs.type === "soup") {
+    return (
+      <>
+        <label htmlFor="spiciness">Spiciness</label>
+        <input
+          id="spiciness"
+          type="number"
+          min={1}
+          max={10}
+          placeholder="Spiciness scale"
+          onChange={handleSpicinessChange}
+          required
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <label htmlFor="bread">Slices of Bread</label>
+        <input
+          id="bread"
+          type="number"
+          min={1}
+          max={10}
+          placeholder="Slices of Bread"
+          onChange={handleBreadChange}
+          required
+        />
+      </>
+    );
+  }
+};
